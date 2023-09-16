4ae12cd38fd5145ae3b2b65cfe072fd86714d997
bipin-shrestha <iambpn.shrestha@gmail.com>
2023-09-16 13:49:10 -0700

    Module 1 Critical Thinking

---
+++ module1.java
@@ -0,0 +1,14 @@
+//Define Class module
+public class module1 {
+	//Define the main method
+	public static void main(String[] args) {
+
+		// Print the user information
+		System.out.println("First Name: Bruce");
+		System.out.println("Last Name: Wayne" );
+		System.out.println("Street Address: 224 Park Drive");
+		System.out.println("City: Gotham City" );
+		System.out.println("Zip Code: 1007" );
+		
+	}
+}
